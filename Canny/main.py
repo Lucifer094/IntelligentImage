309593b90feb08ea369f0a8065fcc4be914d89ec
Lucifer094 <769903065@qq.com>
2024-05-08 22:16:52 +0800

    init

--- Canny/main.py
+++ Canny/main.py
@@ -4,6 +4,9 @@ import numpy as np
 import math
 
 img = cv2.imread('Lena.png')  # Read image data.
+# plt.imshow(img, cmap="gray")
+# plt.title("Original image")
+# plt.show()
 ########## Begin ##########
 
 # 1.Using Gaussian function to smooth image.
@@ -11,131 +14,163 @@ img = cv2.imread('Lena.png')  # Read image data.
 sigma1 = sigma2 = 1
 gau_sum = 0
 gua_win = 5
-gua_half_win = 3
+gua_half_win = 2
 gaussian = np.zeros([gua_win, gua_win])
 for i in range(gua_win):
     for j in range(gua_win):
         gaussian[i, j] = math.exp(-1/2 * (np.square(i-gua_half_win)/np.square(sigma1) + (np.square(j-gua_half_win)/np.square(sigma2)))) / \
                          (2*math.pi*sigma1*sigma2)
-        gau_sum = gau_sum + gaussian[i, j]
-
+gau_sum = np.sum(gaussian)
+gaussian = gaussian/gau_sum
 # 1.2.Product image and Gaussian matrix.
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 W, H = gray.shape
+new_gray = np.zeros([W, H])
+for i in range(gua_half_win, W-gua_half_win):
+    for j in range(gua_half_win, H-gua_half_win):
+        new_gray[i, j] = np.sum(gray[i-gua_half_win:i+gua_half_win+1, j-gua_half_win:j+gua_half_win+1]*gaussian)
+# plt.imshow(new_gray, cmap="gray")
+# plt.title("Gaussian smooth")
+# plt.show()
 
+# 2.Pixel gradient calculation.
+IX = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+IY = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+GX = np.zeros([W, H])
+GY = np.zeros([W, H])
+GXY = np.zeros([W, H])
+for i in range(gua_half_win, W-gua_half_win):
+    for j in range(gua_half_win, H-gua_half_win):
+        GX[i, j] = np.sum(new_gray[i-1:i+2, j-1:j+2]*IX)
+        GY[i, j] = np.sum(new_gray[i-1:i+2, j-1:j+2]*IY)
+        GXY[i, j] = math.sqrt(pow(GX[i, j], 2) + pow(GY[i, j], 2))
+# plt.imshow(GX, cmap="gray")
+# plt.title("GX")
+# plt.show()
+# plt.imshow(GY, cmap="gray")
+# plt.title("GY")
+# plt.show()
+# plt.imshow(GXY, cmap="gray")
+# plt.title("GXY")
+# plt.show()
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-
-# step1.高斯滤波
-gray = rgb2gray(img)
-W, H = gray.shape
-new_gray = np.zeros([W-5, H-5])
-for i in range(W-5):
-    for j in range(H-5):
-        new_gray[i,j] = np.sum(gray[i:i+5,j:j+5]*gaussian)   # 与高斯矩阵卷积实现滤波
-
-plt.imshow(new_gray, cmap="gray")
-
-# step2.增强 通过求梯度幅值
-W1, H1 = new_gray.shape
-dx = np.zeros([W1 - 1, H1 - 1])
-dy = np.zeros([W1 - 1, H1 - 1])
-d = np.zeros([W1 - 1, H1 - 1])
-for i in range(W1 - 1):
-    for j in range(H1 - 1):
-        dx[i, j] = new_gray[i, j + 1] - new_gray[i, j]
-        dy[i, j] = new_gray[i + 1, j] - new_gray[i, j]
-        d[i, j] = np.sqrt(np.square(dx[i, j]) + np.square(dy[i, j]))  # 图像梯度幅值作为图像强度值
-
-plt.imshow(d, cmap="gray")
-
-# setp3.非极大值抑制 NMS
-W2, H2 = d.shape
-NMS = np.copy(d)
-NMS[0, :] = NMS[W2 - 1, :] = NMS[:, 0] = NMS[:, H2 - 1] = 0
-for i in range(1, W2 - 1):
-    for j in range(1, H2 - 1):
-
-        if d[i, j] == 0:
+# 3.Non-maximum suppression.
+NMS = np.copy(GXY)
+for i in range(gua_half_win, W-gua_half_win):
+    for j in range(gua_half_win, H-gua_half_win):
+        if GXY[i, j] == 0:
             NMS[i, j] = 0
         else:
-            gradX = dx[i, j]
-            gradY = dy[i, j]
-            gradTemp = d[i, j]
-
-            # 如果Y方向幅度值较大
+            gradX = GX[i, j]
+            gradY = GY[i, j]
+            gradTemp = GXY[i, j]
             if np.abs(gradY) > np.abs(gradX):
                 weight = np.abs(gradX) / np.abs(gradY)
-                grad2 = d[i - 1, j]
-                grad4 = d[i + 1, j]
-                # 如果x,y方向梯度符号相同
+                grad2 = GXY[i - 1, j]
+                grad4 = GXY[i + 1, j]
                 if gradX * gradY > 0:
-                    grad1 = d[i - 1, j - 1]
-                    grad3 = d[i + 1, j + 1]
-                # 如果x,y方向梯度符号相反
+                    grad1 = GXY[i - 1, j - 1]
+                    grad3 = GXY[i + 1, j + 1]
                 else:
-                    grad1 = d[i - 1, j + 1]
-                    grad3 = d[i + 1, j - 1]
-
-            # 如果X方向幅度值较大
+                    grad1 = GXY[i - 1, j + 1]
+                    grad3 = GXY[i + 1, j - 1]
             else:
                 weight = np.abs(gradY) / np.abs(gradX)
-                grad2 = d[i, j - 1]
-                grad4 = d[i, j + 1]
-                # 如果x,y方向梯度符号相同
+                grad2 = GXY[i, j - 1]
+                grad4 = GXY[i, j + 1]
                 if gradX * gradY > 0:
-                    grad1 = d[i + 1, j - 1]
-                    grad3 = d[i - 1, j + 1]
-                # 如果x,y方向梯度符号相反
+                    grad1 = GXY[i + 1, j - 1]
+                    grad3 = GXY[i - 1, j + 1]
                 else:
-                    grad1 = d[i - 1, j - 1]
-                    grad3 = d[i + 1, j + 1]
-
+                    grad1 = GXY[i - 1, j - 1]
+                    grad3 = GXY[i + 1, j + 1]
             gradTemp1 = weight * grad1 + (1 - weight) * grad2
             gradTemp2 = weight * grad3 + (1 - weight) * grad4
             if gradTemp >= gradTemp1 and gradTemp >= gradTemp2:
                 NMS[i, j] = gradTemp
             else:
                 NMS[i, j] = 0
+# plt.imshow(NMS, cmap="gray")
+# plt.title("NMS")
+# plt.show()
 
-plt.imshow(NMS, cmap = "gray")
-
-# step4. 双阈值算法检测、连接边缘
-W3, H3 = NMS.shape
-DT = np.zeros([W3, H3])
-# 定义高低阈值
-TL = 0.2 * np.max(NMS)
-TH = 0.3 * np.max(NMS)
-for i in range(1, W3-1):
-    for j in range(1, H3-1):
-        if (NMS[i, j] < TL):
-            DT[i, j] = 0
-        elif (NMS[i, j] > TH):
-            DT[i, j] = 1
-        elif ((NMS[i-1, j-1:j+1] < TH).any() or (NMS[i+1, j-1:j+1]).any()
-              or (NMS[i, [j-1, j+1]] < TH).any()):
-            DT[i, j] = 1
-plt.imshow(DT,cmap="gray")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#
-# blur = cv2.GaussianBlur(img, (5, 5), 0)  # 用高斯滤波处理原图像降噪
-# canny = cv2.Canny(blur, 50, 150)  # 50是最小阈值,150是最大阈值
-#
-#
-# plt.figure(1)
-# ax1 = plt.subplot(1, 2, 1)
-# plt.sca(ax1)
-# plt.imshow(img)
-# plt.title("artwork")
-# #第一行第二列图形
-# ax2 = plt.subplot(1,2,2)
-# plt.sca(ax2)
-# plt.imshow(canny, cmap="gray")
-# plt.title("opencv Canny")
+# 4. Hysteresis threshold processing.
+TL = 0.05 * np.max(NMS)
+TH = 0.7 * np.max(NMS)
+DTT = 0 * NMS
+for i in range(gua_half_win, W-gua_half_win):
+    for j in range(gua_half_win, H-gua_half_win):
+        if DTT[i, j] == 1:
+            continue
+        else:
+            if (NMS[i, j] < TL):
+                DTT[i, j] = 0
+            elif (NMS[i, j] > TH):
+                DTT[i, j] = 1
+            # else:
+            #     DTT[i, j] = 1
+            elif ((NMS[i-1, j-1:j+1] < TH).any() or (NMS[i+1, j-1:j+1]).any()
+                  or (NMS[i, [j-1, j+1]] < TH).any()):
+                DTT[i, j] = 1
+plt.imshow(DTT, cmap="gray")
+plt.title("DTT")
 plt.show()
+
+
+# # 5. Isolated weak edge suppression.
+# DT = 0 * NMS
 #
 #
+# def search_zero(r, c, d):
+#     if r <= gua_half_win*4 or r >= W-gua_half_win*4 or \
+#             c <= gua_half_win*4 or c >= H-gua_half_win*4:
+#         return 1
+#     elif np.sum(DTT[r-1:r+2, c-1:c+2]) <= 2:
+#         return 1
+#     elif np.sum(DT[r-1:r+2, c-1:c+2]) >= 9:
+#         return 1
+#     elif d < 1:
+#         return 1
+#     else:
+#         for m in range(-1, 2):
+#             for n in range(-1, 2):
+#                 if DTT[r+m, c+n] > 0:
+#                     DT[r+m, c+n] = 1
+#                     DTT[r+m, c+n] = 2
+#                     search_zero(r+m, c+n, d-1)
+#                 else:
+#                     continue
+#         return 0
 #
-# ########## End ##########
-# # cv2.imwrite('canny/result/out.png', out)
+#
+# # for p in range(1, 10):
+# for i in range(gua_half_win*2, W-gua_half_win*2):
+#     for j in range(gua_half_win*2, H-gua_half_win*2):
+#         if DTT[i, j] == 2:
+#             DT[i, j] = 1
+#             search_zero(i, j, 3)
+#         elif DTT[i, j] == 1:
+#             for m in range(-1, 2):
+#                 for n in range(-1, 2):
+#                     if DTT[i+m, j+n] == 2:
+#                         DT[i, j] = 1
+#                         DTT[i, j] = 2
+#                         search_zero(i, j, 3)
+#                         break
+#                     else:
+#                         continue
+#         else:
+#             continue
+#
+# plt.imshow(DT, cmap="gray")
+# plt.title("DT")
+# plt.show()
+
+out = np.copy(DTT)
+out = out * 255
+out[1:gua_win, 1:H] = 0
+out[W-gua_win:W, 1:H] = 0
+out[1:W, 1:gua_win] = 0
+out[1:W, H-gua_win:H] = 0
+########## End ##########
+cv2.imwrite('out.png', out)
--- README.md
+++ README.md
@@ -21,5 +21,57 @@ $$
 
 ## 2.1. Introduce:
 
+Canny edge detection is one of the image segmentation method. Canny edge detection is to find the position of the largest gradient change in the image, so as to determine the position of the edge.
+
 ## 2.2.Processing:
 
+1. Gaussian filtering: Use two-dimensional Gaussian function create one gaussian filter which size is 3*3 and standard deviation is 1. Image multiplied the gaussian filter to smooth the image data.
+
+$$
+G(x,y) = \frac{1}{2\pi \sigma ^ {2}} exp(-\frac{x^{2}+y^{2}} {2\sigma^{2}})
+$$
+
+$$
+\begin{bmatrix}0.0751136&0.1238414&0.0751136\\
+0.1238414&0.2041799&0.1238414\\
+0.0751136&0.1238414&0.0751136\end{bmatrix}
+$$
+
+2. Pixel gradient calculation: Using Sobel Operator calculate each direction gradient value, total gradient value and gradient direction.
+
+- Each direction gradient value:
+
+$$
+G_{x}=S_{x}\ast I=
+\begin{bmatrix}-1&0&+1\\
+-2&0&+2\\
+-1&0&+1\end{bmatrix}\ast I
+$$
+$$
+G_{y}=
+S_{y}\ast I=
+\begin{bmatrix}-1&-2&-1\\
+0&0&0\\
++1&+2&+1\end{bmatrix}\ast I
+$$
+
+3. Non-maximum suppression: Traverse all points on the gradient matrix and preserve pixels with maximum values in the edge direction.
+
+![Non-maximum suppression](img/Non-maximum%20suppression.jpg)
+
+- For more accurate calculation, linear interpolation is usually used between two adjacent pixels spanning the gradient direction to obtain the pixel gradient to be compared.
+
+
+$$
+g_{up}(i,j)=(1-t) \cdot g_{xy}(i,j+1)+t \cdot g_{xy}(i-1,j+1)
+$$
+
+$$
+g_{down} \left(i,j \right)=(1-t) \cdot g_{xy}(i,j-1)+t \cdot g_{xy}(i+1,j-1)
+$$
+
+4. Hysteresis threshold processing:
+5. Isolated weak edge suppression:
+
+
+[Reference link](https://zhuanlan.zhihu.com/p/99959996)
